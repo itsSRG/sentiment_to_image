556b0babe9bc48ee60c1e3c244021ff5c27db707
SRG <itsmesrghere@gmail.com>
2022-04-13 10:17:04 +0530

    renamed image

--- sentiment.py
+++ sentiment.py
@@ -11,17 +11,8 @@ from PIL import Image, ImageEnhance
 import random
 
 
-img_neu = Image.open("vines1.png")
-img_neu = img_neu.resize((250,250))
-
 from PIL import Image, ImageEnhance
 collage = Image.new("RGB", (2500,2500), (255,255,255))
-
-img_pos = Image.open("f11.png")
-img_pos = img_pos.resize((250,250))
-
-img_neg = Image.open("f22.png")
-img_neg = img_neg.resize((250,250))
 
 c=0.7
 row = [i for i in range (10)]
@@ -38,7 +29,9 @@ print(random_row_pos)
 
 for i in range(0, 2500, 250):
     for j in range(0, 2500, 250):
-        
+        image_list = ['neu1.png','neu2.png','neu3.png','neu4.png', 'neu5.png', 'neu6.png', 'neu7.png']
+        img_neu = Image.open(random.choice(image_list))
+        img_neu = img_neu.resize((250,250))
         temp = random.randint(0,100)
         if temp < 25:
             rot_image = img_neu.rotate(90)
@@ -51,25 +44,35 @@ for i in range(0, 2500, 250):
             collage.paste(rot_image, (j,i))
         else:
             collage.paste(img_neu, (j,i))
-            
+        img_neu.close()
 
 for i in range(0, len(random_row_pos)):
-    new_img = img_pos
+    image_list = ['fh1.png','fh2.png','fh3.png','fh4.png']
+    img_pos = Image.open(random.choice(image_list))
+    img_pos = img_pos.resize((250,250))
     temp = random.randint(0,100)
     if temp < 25:
-        new_img = img_pos.rotate(90)
-        # collage.paste(rot_image, (j,i))
+        img_pos = img_pos.rotate(90)
     elif temp < 50:
-        new_img = img_pos.rotate(180)
-        # collage.paste(rot_image, (j,i))
+        img_pos = img_pos.rotate(180)
     elif temp < 75:
-        new_img = img_pos.rotate(270)
-        # collage.paste(rot_image, (j,i))
-    collage.paste(new_img, (random_column_pos[i]*250, random_row_pos[i]*250))
+        img_pos = img_pos.rotate(270)
+    collage.paste(img_pos, (random_column_pos[i]*250, random_row_pos[i]*250))
+    img_pos.close()
 
 c = 0.7
 for i in range(0, len(random_row_neg)):
-    # new_img = colour.enhance(0)
+    image_list = ['fs1.png']
+    img_neg = Image.open(random.choice(image_list))
+    img_neg = img_pos.resize((250,250))
+    temp = random.randint(0,100)
+    if temp < 25:
+        img_neg = img_neg.rotate(90)
+    elif temp < 50:
+        img_neg = img_neg.rotate(180)
+    elif temp < 75:
+        img_neg = img_neg.rotate(270)
     collage.paste(img_neg, (random_column_neg[i]*250, random_row_neg[i]*250))
+    img_neg.close()
 
 collage.show()
